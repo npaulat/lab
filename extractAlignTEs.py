217e0fc9e842b818b6c49dcb94705c0854b56fb0
npaulat <34585535+npaulat@users.noreply.github.com>
2019-04-23 11:20:31 -0500

    Update extractAlignTEs.py

--- extractAlignTEs.py
+++ extractAlignTEs.py
@@ -5,7 +5,7 @@ import re
 import subprocess
 import pandas as pd
 from Bio import SearchIO
-for Bio import SeqIO
+from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.Alphabet import generic_dna
 
@@ -73,14 +73,30 @@ def CREATE_TE_OUT_FILES(CONS_TES):
 			#SeqIO.write(SEQ, OUTPUT, "fasta")
 			OUTPUT.write(">CONSENSUS-" + SEQ_ID + "\n" + SEQ + "\n")
 	
-#def ORGANIZE_BLAST_HITS(BLAST_HITS, SEQ_BUFFER, SEQ_NUM):
-	
-	
-	
-	
-	
-	
-	
-	
+def ORGANIZE_BLAST_HITS(BLAST_HITS, SEQ_BUFFER, SEQ_NUM):
+## Read in blast data
+	BLAST_DATA = pd.read_table(BLAST, sep='\t', names=['QUERYNAME', 'SCAFFOLD', 'C', 'D', 'E', 'F', 'QUERYSTART', 'QUERYSTOP', 'SCAFSTART', 'SCAFSTOP', 'E-VALUE', 'BITSCORE'])
+## Convert to bed format
+	BLASTBED = BLAST_DATA[['SCAFFOLD', 'SCAFSTART', 'SCAFSTOP', 'QUERYNAME', 'E-VALUE', 'BITSCORE']]
+	BLASTBED.insert(4, 'STRAND', '+')
+	BLASTBED.loc[BLASTBED.SCAFSTOP < BLASTBED.SCAFSTART, 'STRAND'] = '-'
+	BLASTBED.SCAFSTART, BLASTBED.SCAFSTOP = np.where(BLASTBED.SCAFSTART > BLASTBED.SCAFSTOP, [BLASTBED.SCAFSTOP, BLASTBED.SCAFSTART], [BLASTBED.SCAFSTART, BLASTBED.SCAFSTOP])
+## Generate list of query names
+	QUERYLIST = BLASTBED.QUERYNAME.unique()	
+## For each query, sort hits
+	for QUERY in QUERYLIST:
+		QUERY_HITS = BLASTBED[BLASTBED['QUERYNAME' == QUERY].sort_values['E-VALUE', 'BITSCORE'], ascending=[True, False])
+		QUERY_HITS.reset_index(inplace=True, drop=True)
+	## Select top SEQ_NUM hits, output QUERY BED file
+		QUERY_HITS = QUERY_HITS.head(SEQ_NUM)
+		OUT_FILE = QUERY + ".bed"
+		OUTPUT = os.path.join(OUTDIR, OUT_FILE)
+		QUERY_HITS.to_csv(OUTPUT, sep='\t', header=True)
+	## Add BUFFER to SCAFSTART, SCAFEND, extract sequences, generate BLAST_HITS FASTA file
+		QUERY_BUFFS = QUERY_HITS[QUERY_HITS.loc[QUERY_HITS.SCAFSTART, 'SCAFSTART'] = SCAFSTART + BUFFER]
+		QUERY_BUFFS = QUERY_HITS[QUERY_HITS.loc[QUERY_HITS.SCAFSTOP, 'SCAFSTOP'] = SCAFSTOP + BUFFER]
+		QUERY_COORDS = QUERY_BUFFS[['SCAFFOLD', 'SCAFSTART', 'SCAFSTOP']].values.tolist()
+		for HIT in QUERY_COORDS:
+			SeqIO.
 	
 	
